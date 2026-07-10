3cd59c9aa6cce6e22a83f1983dec5c5b7281e1f6
Raana <raanaafkari96@gmail.com>
2026-07-10 16:43:32 +0200

    feat: add Person class

---
+++ src/Person.java
@@ -0,0 +1,17 @@
+public class Person {
+    private String name;
+    private String surname;
+    private String identityNumber;
+    private int birthYear;
+
+    public Person(String name, String surname, String identityNumber, int birthYear) {
+        this.name = name;
+        this.surname = surname;
+        this.identityNumber = identityNumber;
+        this.birthYear = birthYear;
+    }
+
+    public int getAge(int currentYear) {
+        return currentYear - this.birthYear;
+    }
+}
